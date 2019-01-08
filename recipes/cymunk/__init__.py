8d4b9c79782dce9a05337f9a6db295ebe49547cd
Thong Tran <ththong@sk-blobal.biz>
2019-01-08 16:58:42 +0700

    Fix build Cymunk

--- recipes/cymunk/__init__.py
+++ recipes/cymunk/__init__.py
@@ -10,11 +10,13 @@ import sh
 
 class CymunkRecipe(CythonRecipe):
     version = 'master'
-    url = 'https://github.com/tito/cymunk/archive/{version}.zip'
+    url = 'https://github.com/kivy/cymunk/archive/{version}.zip'
     name = 'cymunk'
+    library = "libcymunk.a"
     depends = ['hostpython']
     cythonize = True
 
+    '''
     def build_arch(self, arch):
         """
         Override build.arch to avoid calling setup.py here (Call it in 
@@ -22,8 +24,8 @@ class CymunkRecipe(CythonRecipe):
         """
         self.cythonize_build()
         self.biglink()
-
-        
+    '''
+    
     def install(self):
         """
         Do the equivalent of
@@ -36,8 +38,8 @@ class CymunkRecipe(CythonRecipe):
         hostpython = sh.Command(self.ctx.hostpython)
         subdir_path = self.get_build_dir(arch.arch)
         setup_path = join(subdir_path,"setup.py")
-        dest_dir = join (self.ctx.dist_dir, "root", "python")
-        build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python2.7', 'site-packages')
+        dest_dir = join (self.ctx.dist_dir, "root", "python3")
+        build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python3.7', 'site-packages')
 
         #Note: Throws error if PATH is not set. I am not sure if this will cause problems
         # in other architectures.
@@ -48,6 +50,7 @@ class CymunkRecipe(CythonRecipe):
                 "build_ext",
                 #"--compiler=mingw32", #note: throws clang error
                 "install",
+                "--prefix", dest_dir,
                 _env=build_env)
-    
+
 recipe = CymunkRecipe()
