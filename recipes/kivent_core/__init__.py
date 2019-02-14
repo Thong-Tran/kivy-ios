e4e718c1f8bfeb56edcef9d16c9b9c99e53951b0
Thong Tran <ththong@sk-blobal.biz>
2019-02-14 10:09:38 +0700

    Update recipe: KivEnt

--- recipes/kivent_core/__init__.py
+++ recipes/kivent_core/__init__.py
@@ -5,8 +5,9 @@ E-mail: larrydu88@gmail.com
 
 from toolchain import CythonRecipe, shprint
 import sh
-from os.path import join
-from os import environ, chdir
+from os.path import join, abspath
+from os import environ, chdir, walk, getcwd
+import fnmatch
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ class KiventCoreRecipe(CythonRecipe):
     version = 'master'
     url = 'https://github.com/kivy/kivent/archive/{version}.zip'
     name = 'kivent_core'
+    library = "libkivent_core.a"
     depends = ['libffi','kivy'] #note: unsure if libffi is necessary here
     pre_build_ext=False
     subbuilddir = False
@@ -25,8 +27,12 @@ class KiventCoreRecipe(CythonRecipe):
 
     def get_recipe_env(self, arch):
         env = super(KiventCoreRecipe,self).get_recipe_env(arch)
+        env['PATH'] = env.get('PATH', '')
         env['CYTHONPATH'] = self.get_recipe(
             'kivy', self.ctx).get_build_dir(arch.arch)
+        dest_dir = join (self.ctx.dist_dir, "root", "python3")
+        env['PYTHONPATH'] = join(dest_dir, 'lib', 'python3.7', 'site-packages')
+
         return env
 
 
@@ -44,17 +50,41 @@ class KiventCoreRecipe(CythonRecipe):
             logger.info("Building in {}".format(builddir))
             return builddir
 
-
     def build_arch(self, arch):
         """
         Override build.arch to avoid calling setup.py here (Call it in
         install() instead).
         """
 
+        build_env = self.get_recipe_env(arch)
+        hostpython = sh.Command(self.ctx.hostpython)
         self.subbuildir = True
-        self.cythonize_build()
+        if self.pre_build_ext:
+            try:
+                shprint(hostpython, "setup.py", "build_ext", "-g",
+                        _env=build_env)
+            except:
+                pass
+
+        cyenv = environ.copy()
+        # cyenv['PATH'] = environ['PATH']
+        cyenv['CYTHONPATH'] = self.get_recipe(
+            'kivy', self.ctx).get_build_dir(arch.arch)
+        dest_dir = join (self.ctx.dist_dir, "root", "python3")
+        cyenv['PYTHONPATH'] = join(dest_dir, 'lib', 'python3.7', 'site-packages')
+        # for i,j in cyenv.items():
+        #     print(i,j, '\n')
+        self.cythonize_build(cyenv, build_dir="./modules/core/kivent_core")
+
+        cwd = getcwd()
+        build_dir = self.get_build_dir(arch.arch,sub=True)
+        logger.info("Building kivent_core {} in {}".format(arch.arch,build_dir))
+        chdir(build_dir)
+        shprint(hostpython, "setup.py", "build_ext", "-g",
+                _env=build_env)
         self.biglink()
         self.subbuilddir=False
+        chdir(cwd)
 
 
     def install(self):
@@ -82,8 +112,8 @@ class KiventCoreRecipe(CythonRecipe):
         #build_env = arch.get_env()
         build_env = self.get_recipe_env(arch)
 
-        dest_dir = join (self.ctx.dist_dir, "root", "python")
-        build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python2.7', 'site-packages')
+        dest_dir = join (self.ctx.dist_dir, "root", "python3")
+        build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python3.7', 'site-packages')
 
         #Add Architecture specific kivy path for 'import kivy' to PYTHONPATH
         arch_kivy_path = self.get_recipe('kivy', self.ctx).get_build_dir(arch.arch)
@@ -103,5 +133,19 @@ class KiventCoreRecipe(CythonRecipe):
         logger.info("ARCH KIVY LOC",self.get_recipe('kivy', self.ctx).get_build_dir(arch.arch))
 
         shprint(hostpython, setup_path, "build_ext", "install", _env=build_env)
+
+    def cythonize_file(self, env, build_dir, filename):
+        short_filename = filename
+        if filename.startswith(build_dir):
+            short_filename = filename[len(build_dir) + 1:]
+        logger.info(u"Cythonize {}".format(short_filename))
+        cmd = sh.Command(join(self.ctx.root_dir, "tools", "cythonize.py"))
+        shprint(sh.Command('python3'), cmd, filename, _env=env)
+
+    def cythonize_build(self, env, build_dir="."):
+        for root, dirnames, filenames in walk(build_dir):
+            for filename in fnmatch.filter(filenames, "*.pyx"):
+                self.cythonize_file(env, build_dir, abspath(join(root, filename)))
+
 
 recipe = KiventCoreRecipe()
